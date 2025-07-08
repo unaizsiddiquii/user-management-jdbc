5aa7424f70e6a59ee1bd4f4d1a775679e2324997
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-07-08 16:12:51 +0530

    db connectionn is done

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,2 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost::3036/userdb", "root", "US0726unaiz@"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
